ffd9a5c1973b7311988993d1cd2d6c456e104ebe
Leticia Tiveron <ltiveron@americastg.com>
2018-06-15 19:20:57 -0300

    adding images + spacing

--- src/components/how_works_region.js
+++ src/components/how_works_region.js
@@ -4,18 +4,16 @@ export default class HowWorksRegion extends Component{
   render(){
     return (
       <section id="how-works-region" className="header-region">
-        <div className="container-fluid">
           <div className="row">
-            <div className="col-md-6 p-100">
+            <div className="col-md-6 padding-50">
               <span className="text-success">COMO FUNCIONA</span>
               <h1>Veja o resultado assim que o gol sair!</h1>
               <span>Abaixo você consegue visualizar o resultado de todos os jogos da Copa do Mundo 2018.</span>
             </div>
             <div className="col-md-6">
-              <img className="img-thumbnail border-0" src="https://images.sportsdirect.com/images/products/08100843_l.jpg"/>
+              <img className="img-thumbnail border-0 m-0 rounded-0 p-0" src="../src/images/img-chuteira.png"/>
             </div>
           </div>
-        </div>
       </section>
     );
   }
--- src/components/share_region.js
+++ src/components/share_region.js
@@ -4,28 +4,28 @@ export default class ShareRegion extends Component{
   render(){
     return (
       <section id="share-region">
-        <div className="share-region bg-success container text-center text-white">
-          <h2>Compartilhe com seus amigos!</h2>
-          <span>Traga seus amigos para ver os resultados dos jogos!</span>
-          <div className="row">
-            <div className="col col-sm-1">
-              fb
-            </div>
-            <div className="col col-sm-1">
-              tw
-            </div>
-            <div className="col col-sm-1">
-              rss
-            </div>
-            <div className="col col-sm-1">
-              inst
-            </div>
-            <div className="col col-sm-1">
-              g+
-            </div>
-          </div>
-          <a className="align-bottom" href="https://www.bolaoshow.com.br/users/sign_in">Bolão da copa</a>
+      <div className="share-region bg-success container text-center text-white">
+      <h2>Compartilhe com seus amigos!</h2>
+      <span>Traga seus amigos para ver os resultados dos jogos!</span>
+      <main className="flex-center">
+        <div className="">
+          <img src="../src/images/icon-facebook.png" href=""/>
         </div>
+        <div className="">
+          <img src="../src/images/icon-twitter.png" href=""/>
+        </div>
+        <div className="">
+          <img src="../src/images/icon-rss.png" href=""/>
+        </div>
+        <div className="">
+          <img src="../src/images/icon-instagram.png" href=""/>
+        </div>
+        <div className="">
+          <img src="../src/images/icon-google.png" href=""/>
+        </div>
+      </main>
+      <a className="align-bottom" href="https://www.bolaoshow.com.br/users/sign_in">Bolão da copa</a>
+      </div>
       </section>
     );
   }
