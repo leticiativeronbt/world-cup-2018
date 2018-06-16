f1d63f7ac61c948015f649d3dd75333c7837467f
leticiativeronbt <leticiativeronbt@gmail.com>
2018-06-16 14:45:35 -0300

    adding link to results

--- src/components/header_region.js
+++ src/components/header_region.js
@@ -6,7 +6,9 @@ export default class HeaderRegion extends Component{
       <section id="header-region" className="header-region">
         <div className="div-with-bg-img bg-info text-white">
           <h1>Acompanhe aqui os resultados da Copa da Russia 2018!</h1>
-          <button className="btn btn-success btn-sm px-4 py-1" href="#group-list">Confira!</button>
+          <button className="btn btn-success btn-sm px-4 py-1">
+            <a className="text-white" href="#group-list">Confira!</a>
+          </button>
         </div>
       </section>
     );
