3a51c7f9cd1b8eaa09cd2576b3db976f2e5a31e6
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-06-11 18:53:04 -0300

    Update

---
+++ precache-manifest.37e666dc172b88f092a8eac1a710eb61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905933acc541f8de8cfe14a54a99323f",
+    "url": "/world-cup-2018/index.html"
+  },
+  {
+    "revision": "d52d041d777a7352730a",
+    "url": "/world-cup-2018/static/css/main.a8c388fe.chunk.css"
+  },
+  {
+    "revision": "8b0278a9ee309f2900aa",
+    "url": "/world-cup-2018/static/js/2.ba83b26d.chunk.js"
+  },
+  {
+    "revision": "d52d041d777a7352730a",
+    "url": "/world-cup-2018/static/js/main.2a064626.chunk.js"
+  },
+  {
+    "revision": "5b4c7abeffb9f4eba377",
+    "url": "/world-cup-2018/static/js/runtime~main.bb923f13.js"
+  },
+  {
+    "revision": "7534cfd369eaa587d261179328836dc4",
+    "url": "/world-cup-2018/static/media/img-chuteira.7534cfd3.png"
+  },
+  {
+    "revision": "74360c799cf72b2f3dd1d6f4c3bb2cff",
+    "url": "/world-cup-2018/static/media/img-hero.74360c79.png"
+  },
+  {
+    "revision": "0fe578be2655b778c9dbcb405461281f",
+    "url": "/world-cup-2018/static/media/logo.0fe578be.svg"
+  }
+]);
